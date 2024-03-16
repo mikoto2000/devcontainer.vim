82836d47ee77729e6193eedfc20a025e673257aa
mikoto2000 <mikoto2000@gmail.com>
2024-03-17 05:51:56 +0900

    Vim の AppImage ダウンロード処理を実装。

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
 	"path"
 )
 
 const APP_NAME = "devcontainer.vim"
+const VIM_TAG_NAME = "v9.1.0181"
+const VIM_DOWNLOAD_URL = "https://github.com/vim/vim-appimage/releases/download/%s/"
+const VIM_FILE_NAME = "Vim-%s.glibc2.29-x86_64.AppImage"
 
 type GetDirFunc func() (string, error)
 
@@ -28,12 +33,26 @@ func main() {
 	// 2. ユーザーキャッシュ用ディレクトリ
 	//    `os.UserCacheDir` + `devcontainer.vim`
 	createDirectory(os.UserConfigDir, APP_NAME)
-	createDirectory(os.UserCacheDir, APP_NAME)
+	appCacheDir := createDirectory(os.UserCacheDir, APP_NAME)
 
 	// vim-appimage のダウンロード
 	// 1. ユーザーキャッシュディレクトリ取得
 	// 2. appimage がダウンロード済みかをチェックし、
 	//    必要であればダウンロード
+	vimFileName := fmt.Sprintf(VIM_FILE_NAME, VIM_TAG_NAME)
+	vimFilePath := path.Join(appCacheDir, vimFileName)
+
+	if isExistsVimAppImage(vimFilePath) {
+		fmt.Printf("Vim AppImage aleady exist, use %s.\n", vimFilePath)
+	} else {
+		vimDownloadUrl := fmt.Sprintf(VIM_DOWNLOAD_URL+vimFileName, VIM_TAG_NAME)
+		fmt.Printf("Download Vim AppImage from %s ...", vimDownloadUrl)
+		err := downloadVimAppImage(vimDownloadUrl, appCacheDir, vimFileName)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(" done.\n")
+	}
 
 	// バックグラウンドでコンテナを起動
 	// `docker run -d --rm os.Args[1:] sh -c "sleep infinity"`
@@ -58,7 +77,7 @@ func isExistsCommand(command string) bool {
 	return true
 }
 
-func createDirectory(pathFunc GetDirFunc, dirName string) {
+func createDirectory(pathFunc GetDirFunc, dirName string) string {
 	var baseDir, err = pathFunc()
 	if err != nil {
 		panic(err)
@@ -67,4 +86,32 @@ func createDirectory(pathFunc GetDirFunc, dirName string) {
 	if err := os.MkdirAll(appCacheDir, 0766); err != nil {
 		panic(err)
 	}
+	return appCacheDir
+}
+
+func isExistsVimAppImage(vimFilePath string) bool {
+	_, err := os.Stat(vimFilePath)
+	return err == nil
+}
+
+func downloadVimAppImage(vimDownloadUrl string, appCacheDir string, vimFileName string) error {
+	vimFilePath := path.Join(appCacheDir, vimFileName)
+
+	// HTTP GETリクエストを送信
+	resp, err := http.Get(vimDownloadUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// ファイルを作成
+	out, err := os.Create(vimFilePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// レスポンスの内容をファイルに書き込み
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
