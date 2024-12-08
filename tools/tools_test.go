9a73aa9343a5ea2dc7bc188da7f98afccd973c54
mikoto2000 <mikoto2000@gmail.com>
2024-12-08 02:15:14 +0000

    Added TestInstallStartTools.

---
+++ tools/tools_test.go
@@ -0,0 +1,44 @@
+package tools
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/mikoto2000/devcontainer.vim/v3/util"
+)
+
+func TestInstallStartTools(t *testing.T) {
+	defer os.RemoveAll("test")
+	_, binDir, _, _, err := util.CreateCacheDirectory(func() (string, error) {
+		return "test", nil
+	}, "resource")
+	if err != nil {
+		panic(err)
+	}
+
+	devcontainerPath, cdrPath, err := InstallStartTools(binDir)
+	if err != nil {
+		t.Fatalf("Error installing start tools: %v", err)
+	}
+
+	// devcontainer の存在確認
+	wantDevcontainerPath := filepath.Join(binDir, "devcontainer")
+	if wantDevcontainerPath != devcontainerPath {
+		t.Fatalf("want %s, but got %s", wantDevcontainerPath, devcontainerPath)
+	}
+	_, err = os.Stat(wantDevcontainerPath)
+	if err != nil {
+		t.Fatalf("error: %s", err)
+	}
+	// cdr の存在確認
+	wantCdrPath := filepath.Join(binDir, "clipboard-data-receiver")
+	if wantCdrPath != cdrPath {
+		t.Fatalf("want %s, but got %s", wantCdrPath, cdrPath)
+	}
+	_, err = os.Stat(wantCdrPath)
+	if err != nil {
+		t.Fatalf("error: %s", err)
+	}
+}
+
