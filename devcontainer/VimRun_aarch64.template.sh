2e217ff191358798dd0a36361dbb1eae840c1153
mikoto2000 <mikoto2000@gmail.com>
2026-03-22 00:10:28 +0000

    tmux の OSC52 対応。

--- devcontainer/VimRun_aarch64.template.sh
+++ devcontainer/VimRun_aarch64.template.sh
@@ -7,4 +7,4 @@ cd ~
 chmod -R +w ~/vim-static
 rm -rf ~/vim-static
 mv $(ls -d ~/vim-*-aarch64) ~/vim-static
-{{ .TmuxCommand }} -u set-option -g status off \; new-session -s "devcontainer.vim" -A ~/vim-static/AppRun --cmd "let g:devcontainer_vim = v:true" -S /{{ .SendToTcp}} -S /vimrc $*
+{{ .TmuxCommand }} -u set-option -g status off \; set-option -g set-clipboard on \; new-session -s "devcontainer.vim" -A ~/vim-static/AppRun --cmd "let g:devcontainer_vim = v:true" -S /{{ .SendToTcp}} -S /vimrc $*
